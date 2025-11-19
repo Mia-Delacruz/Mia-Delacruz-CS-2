3dfe901b9b227ca0ad9de8ab9691e2d7db1573af
Mia-Delacruz <mdelacruz27@gcds.net>
2025-11-19 13:23:21 -0500

    Add files via upload

---
+++ whats in a name_delacruz_cs2.py
@@ -0,0 +1,225 @@
+'''
+*/
+mia_delacruz 
+for each function user gets to pick a number and apply it 
+menu 
+bugs were a lot of syntax and indentation that at first would not run my code 
+Log 1.0 M
+'''
+import random 
+
+def split_string(string):
+    '''
+    for parts in bracket 
+    current equal to ""
+    for character in string 
+    if char is equal to ""
+    append current 
+    if else
+    current more or equal to character 
+    append current 
+    return parts 
+    '''
+    parts = []
+    current = ""
+    
+    for char in string:
+        if char == " ":
+            parts.append(current)
+            current = ""
+        else:
+            current += char
+    parts.append(current)
+    return parts
+
+def get_first(fullname):
+    '''
+    full name equal split string 
+    return names
+    '''
+    names = split_string(fullname)
+    return names[0]
+
+def get_middle(fullname):
+    '''
+    full name equal to split string 
+    return ' ' 
+    join names 1,-1
+    '''
+    names = split_string(fullname)
+    return ' '.join(names[1:-1])
+
+def last_name(fullname):
+    '''
+    full name equal to split string 
+    only return name -1 to user 
+    '''
+    names = split_string(fullname)
+    return names[-1]
+
+def get_initials(fullname):
+    '''
+    use split string to split full name into a last 
+    add first letter of name to initals 
+    return initials 
+    '''
+    initials = ''
+    full_name = name.split ('list')
+    for name in list:
+     first_letter += initials 
+     return initials
+def get_random(string):
+    '''
+    turn string into a list of all letters
+    use randomize shuffle to scarmble all letters in the list
+    insert join to put the scrambled list back into the original string and return to user
+    '''
+    import random 
+    letters = list ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s,','t','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+    random.shuffle (letters)
+    return list (letters) 
+
+def count_vowels(string):  
+    '''
+    Counts total vowels in a string and provides a subtotal for each vowel. 
+
+    Args:
+     Input string
+
+    Returns:
+        Total count of vowels and a dictionary with individual vowel counts.
+    '''                                                 
+    vc = 0                                                              
+    vowels = list('a','e','i','o','u','A','E','I','O','U')                                        
+
+    for char in string:                                                      
+        if char in vowels:                                              
+            vc += 1                                                  
+    return vc             
+  
+
+def count_consonants(string): 
+    '''
+    Counts total consonants in a string and provides a subtotal for each consonant. 
+
+    Args:
+     Input string
+
+    Returns:
+        Total count of consonants and a dictionary with individual consonant counts.
+ ''' 
+    cc = 0 
+    consonants = list('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s,','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z')
+
+    for char in string:
+          if char in consonants:                                              
+                cc += 1                                                  
+          return cc                                                          
+
+def reverse_string(string): 
+    '''
+    reverse string to -1
+    '''
+    return string[::-1]
+
+def hyphen (name):
+    '''
+    return name if hyphen detected
+    '''
+    return '-' in name
+
+def palindrome (name): 
+    '''
+    return name in string
+    reversed 
+    '''
+    return name == reverse_string(name)
+
+def lowercase(string): 
+    '''
+    for letter in string
+    if ord below 64 and 91
+    set number more or equal to 32
+    for letter in character 
+    return upperstring 
+    '''
+    upper_string = ""
+
+    for letter in string: 
+        if ord(letter) > 64 and ord(letter) < 91:
+            num = ord(letter)
+            num += 32
+            letter = chr(num) 
+        upper_string += letter 
+    return upper_string 
+
+def uppercase(string): 
+    '''
+    for letter in string
+    if ord below 96 and 123
+    set number less or equal to 32
+    for letter in character 
+    return upperstring 
+    '''
+    lower_string = ""
+
+    for letter in string: 
+        if ord(letter) > 96 and ord(letter) < 123:
+            num = ord(letter)
+            num -= 32
+            letter = chr(num) 
+        lower_string += letter 
+    return lower_string 
+
+def main ():
+    name = input("Hello user, please enter your name. ")
+    print("Welcome {name}")
+
+    while True:
+        number = input('''pick a function:
+                        1. Reverse and Display 
+                        2. Count Vowels 
+                        3. Count consonants 
+                        4. Return first name 
+                        5. Return last name 
+                        6. Return middle name 
+                        7. Return boolean if last name contains a hyphen 
+                        8. Function to convert to lowercase
+                        9. Function to convert to uppercase  
+                        10.create random name
+                        11.return boolean if first name is palindrome
+                        12.return first name as sorted array of characters
+                        13.build menu (bonus)
+                        14.make initials from name
+                        15.return boolean if name contains a title
+                        16.make own bonus
+                        17.secret bonus
+        ''')
+
+        if number == '1':
+            print(reverse_string(name))
+        elif number == 2:
+            print(count_vowels(name))
+        elif number == 3:
+            print(count_consonants(name))
+        elif number == 4: 
+            print(get_first(name))
+        elif number == 5:
+            print(last_name(name))
+        elif number == 6: 
+            print(get_middle(name))
+        elif number == 7:
+            print(hyphen(name))
+        elif number == 8: 
+            print(lowercase(name))
+        elif number == 9: 
+            print(uppercase(name))
+        elif number == 10:
+            print(get_random(name))
+        elif number == 11:
+            print(palindrome(name))
+        elif number == 14: 
+            print(get_initials(name))
+        
+main()
+
